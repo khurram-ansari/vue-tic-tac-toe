23a0cf213a9ebb6b7e5c0c0c612d093f2e042e3a
khurram-ansari <khury.slm@gmail.com>
2024-03-12 14:28:27 +0400

    Refactored square into separare comp

---
+++ src/components/BoardSquare.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="square">
+    {{ value }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  value?: string
+}>()
+</script>
+
+<style scoped>
+.square {
+  @apply text-xl border-black sm:text-6xl border-2 flex justify-center items-center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BoardSquare from '@/components/BoardSquare.vue'
+</script>
 
 <template>
   <main>
     <section class="min-h-screen flex items-center justify-center px-10">
-      <div
-        class="board grid border-8 border-black grid-cols-3 grid-rows-3 aspect-square w-[100%] max-w-[700px]"
-      >
-        <div
-          v-for="sq in 9"
-          :key="sq"
-          class="square text-red-500 text-xl border-black sm:text-6xl border-2 flex justify-center items-center"
-        >
-          X
-        </div>
+      <div class="board">
+        <BoardSquare v-for="sq in 9" :key="sq" />
       </div>
     </section>
   </main>
 </template>
+<style scoped>
+.board {
+  @apply grid border-8 border-black grid-cols-3 grid-rows-3 aspect-square w-[100%] max-w-[700px];
+}
+</style>
