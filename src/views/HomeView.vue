3ec0fd34935f13eb1e9ee10c08b95993c5874897
khurram-ansari <khury.slm@gmail.com>
2024-03-13 15:07:17 +0400

    added strike on winning

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,13 +7,21 @@ const squares: Ref<any[]> = ref(Array(9).fill(null))
 
 const players: Ref<Player[]> = ref([
   { name: 'Khurram', symbol: 'X', color: '#e70767' },
-  { name: 'John', symbol: 'O', color: '#0000ff' }
+  { name: 'Fatima', symbol: 'O', color: '#0000ff' }
 ])
 
 const activePlayerIndex: Ref<number> = ref(0)
+
 const isGameOver: Ref<boolean> = ref(false)
 
+const winningResult: Ref<any> = ref(null)
+
 const activePlayer = computed(() => players.value[activePlayerIndex.value])
+const winningStrikeClass = computed(() => {
+  const prefix = 'dir-'
+  const dir = winningResult.value?.join('')
+  return dir ? `${prefix}${dir}` : ''
+})
 
 const changeTurn = () => {
   activePlayerIndex.value = (activePlayerIndex.value + 1) % players.value.length
@@ -34,15 +42,18 @@ const checkWin = () => {
     return combination.every((index) => squares.value[index]?.symbol === activePlayer.value.symbol)
   })
 
-  return !!winningCombination
+  return winningCombination
 }
 
 const setSquareValue = (index: number) => {
   if (!squares.value[index] && isGameOver.value === false) {
     squares.value[index] = { symbol: activePlayer.value.symbol, color: activePlayer.value.color }
 
-    if (checkWin()) isGameOver.value = true
-    else changeTurn()
+    const result = checkWin()
+    if (result) {
+      isGameOver.value = true
+      winningResult.value = result
+    } else changeTurn()
   }
 }
 </script>
@@ -58,7 +69,7 @@ const setSquareValue = (index: number) => {
         >
         <span v-if="isGameOver"> Won 🎉</span>
       </h1>
-      <div class="board">
+      <div class="board relative">
         <BoardSquare
           @click="setSquareValue(index)"
           :symbol="sq?.symbol"
@@ -66,6 +77,11 @@ const setSquareValue = (index: number) => {
           v-for="(sq, index) in squares"
           :key="index"
         />
+        <div
+          v-if="winningResult"
+          class="strike h-full w-1 absolute bg-green-500"
+          :class="[winningStrikeClass]"
+        ></div>
       </div>
     </section>
   </main>
@@ -74,5 +90,50 @@ const setSquareValue = (index: number) => {
 <style scoped>
 .board {
   @apply grid border-8 border-black grid-cols-3 grid-rows-3 aspect-square w-[100%] max-w-[700px];
+}
+
+.strike.dir-036,
+.strike.dir-147,
+.strike.dir-258 {
+  transform: translateX(-50%);
+}
+.strike.dir-036 {
+  top: 0;
+  left: calc(1 / 6 * 100%);
+}
+.strike.dir-147 {
+  top: 0;
+  left: calc(3 / 6 * 100%);
+}
+.strike.dir-258 {
+  top: 0;
+  left: calc(5 / 6 * 100%);
+}
+.strike.dir-012,
+.strike.dir-345,
+.strike.dir-678 {
+  left: 50%;
+  transform: rotate(90deg) translate(50%, -50%);
+  transform-origin: top;
+}
+.strike.dir-012 {
+  top: calc(1 / 6 * 100%);
+}
+.strike.dir-345 {
+  top: calc(3 / 6 * 100%);
+}
+.strike.dir-678 {
+  top: calc(5 / 6 * 100%);
+}
+
+.strike.dir-048,
+.strike.dir-246 {
+  left: 50%;
+}
+.strike.dir-048 {
+  transform: rotate(-45deg) scaleY(1.3);
+}
+.strike.dir-246 {
+  transform: rotate(45deg) scaleY(1.3);
 }
 </style>
